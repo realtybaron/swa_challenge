e814b317fce730000d72d798a4708feaa428e2c9
Marc Dugger <mdugger@gmail.com>
2019-08-21 15:27:48 -0500

    remove speed bump

--- app/src/main/java/com/example/RxFeedPresenter.kt
+++ app/src/main/java/com/example/RxFeedPresenter.kt
@@ -9,21 +9,18 @@ import com.example.net.RandomUsers
 import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
 import retrofit2.Response
-import java.util.concurrent.TimeUnit
 
-
-class RxFeedPresenter(view: FeedContract.View, endpoints: RandomUserApi, scheduler: SchedulerProvider) :
-    BasePresenter<FeedContract.View>(view), Presenter {
+class RxFeedPresenter(
+    view: FeedContract.View,
+    endpoints: RandomUserApi,
+    scheduler: SchedulerProvider
+) : BasePresenter<FeedContract.View>(view), Presenter {
 
     private var endpoints = endpoints
     private var scheduler = scheduler
 
     override fun fetchFeed(page: Int, results: Int) {
         val obs = endpoints.get("us", page, "template-android-app", results).singleOrError()
-            .delay(
-                1,
-                TimeUnit.SECONDS
-            ) // slow down to allow progress indicator to show. **WARNING: THIS CAUSES FeedPresenterTest TO FAIL**
             .observeOn(scheduler.ui())
             .subscribeOn(scheduler.io())
             .retryWhen(SingleRetry(10, 1000))
